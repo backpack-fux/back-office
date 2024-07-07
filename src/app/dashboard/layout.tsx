01a7769b1b511215f9cc856deabdc6b8573ea6bb
Thomas Humphreys <thomasehumphreys@gmail.com>
2024-07-07 13:59:32 +0200

    fix: build errors

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,23 +1,25 @@
 import { Link } from "@nextui-org/link";
-
+import { NeynarProvider } from "@/providers/neynar";
 import { Navbar } from "@/components/navbar";
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="relative flex flex-col h-screen">
-      <Navbar />
-      <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">{children}</main>
-      <footer className="w-full flex items-center justify-center py-3">
-        <Link
-          isExternal
-          className="flex items-center gap-1 text-current"
-          href="https://backpack.network"
-          title="backpack homepage"
-        >
-          <span className="text-default-600">Blood spilled by</span>
-          <p className="text-primary">Backpack</p>
-        </Link>
-      </footer>
-    </div>
+    <NeynarProvider>
+      <div className="relative flex flex-col h-screen">
+        <Navbar />
+        <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">{children}</main>
+        <footer className="w-full flex items-center justify-center py-3">
+          <Link
+            isExternal
+            className="flex items-center gap-1 text-current"
+            href="https://backpack.network"
+            title="backpack homepage"
+          >
+            <span className="text-default-600">Blood spilled by</span>
+            <p className="text-primary">Backpack</p>
+          </Link>
+        </footer>
+      </div>
+    </NeynarProvider>
   );
 }
