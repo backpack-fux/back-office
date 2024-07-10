dfee6d5e4b3796f85e40de3c062deee1be03548f
Thomas Humphreys <thomasehumphreys@gmail.com>
2024-07-10 21:22:31 +0200

    fix: reload not push

--- src/components/sown.tsx
+++ src/components/sown.tsx
@@ -3,16 +3,14 @@
 import { Button } from "@nextui-org/button";
 import { useNeynarContext } from "@neynar/react";
 import Cookies from "js-cookie";
-import { useRouter } from "next/navigation";
 
 export const SignOutButton = () => {
   const { logoutUser } = useNeynarContext();
-  const router = useRouter();
 
   const handleSignOut = () => {
-    Cookies.remove("pyv2_auth_token");
     logoutUser();
-    router.push("/");
+    Cookies.remove("pyv2_auth_token");
+    window.location.reload();
   };
 
   return (
