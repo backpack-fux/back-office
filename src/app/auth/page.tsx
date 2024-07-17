24d7e90ba670bb957c45be3efd8b261c672b5e4e
Thomas Humphreys <thomasehumphreys@gmail.com>
2024-07-17 20:54:39 +0200

    fix: cookie setter

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -27,7 +27,7 @@ export default function SignInPage() {
         const response = await pylonService.generateAccessToken(signer_uuid, fid);
 
         if (response.message === "success") {
-          // window.location.reload();
+          window.location.reload();
         } else {
           console.error("Failed to generate token:", response);
         }
